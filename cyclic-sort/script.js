aec537167834b582ede03aabb5ce68062ca2e99c
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-08-10 21:41:41 +0700

    Cyclit Sort

--- cyclic-sort/script.js
+++ cyclic-sort/script.js
@@ -1,4 +1,5 @@
-function CyclicSort(i = 0, nums) {
+function CyclicSort(nums) {
+  let i = 0;
   while (i < nums.length) {
     let j = nums[i] - 1;
     nums[i] !== nums[j] ? ([nums[i], nums[j]] = [nums[j], nums[i]]) : (i += 1);
@@ -6,4 +7,22 @@ function CyclicSort(i = 0, nums) {
   return nums;
 }
 
-console.log([3, 1, 5, 4, 2]);
+CyclicSort([3, 1, 5, 4, 2]); // [1, 2, 3, 4, 5]
+
+// Find the Missing Number
+
+function FindMissingNumber(nums) {
+  let i = 0;
+  while (i < nums.length) {
+    let j = nums[i];
+    nums[i] < nums.length && nums[i] !== nums[j]
+      ? ([nums[i], nums[j]] = [nums[j], nums[i]])
+      : (i += 1);
+  }
+
+  for (let i = 0; i < nums.length; i++) if (nums[i] !== i) return i;
+
+  return nums;
+}
+
+FindMissingNumber([4, 0, 3, 1]); // 2
