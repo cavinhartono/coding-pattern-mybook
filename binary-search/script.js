34e4c036ac974280d541d9c038fe05e90db1f796
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-08-31 19:33:24 +0700

    Binary Search

--- binary-search/script.js
+++ binary-search/script.js
@@ -5,19 +5,22 @@ const BinarySearch = (arr, get) => {
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
 
+    // Mencari nilai untuk mencocokan dalam array
     if (get === arr[mid]) return console.log(`Ditemukan pada indeks ke ${mid}`);
 
+    // Mencari sisi kanan
     if (get > arr[mid]) {
       console.log("Mencari pada sisi kanan");
       start = mid + 1;
     }
 
+    // Mencari sisi kiri
     if (get < arr[mid]) {
       console.log("Mencari pada sisi kiri");
       end = mid - 1;
     }
   }
-  console.log("Nilai tidak ditemukan");
+  console.log("Nilai tidak ditemukan"); // Jika tidak ditemukan
 };
 
 let numbers = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
